b0c19e4194ec3ea5242abd9cce6ac6f2ffbb5ae4
pietrzakadrian <contact.pietrzakadrian@gmail.com>
2020-07-13 00:52:53 +0200

    iOS bugfixes

--- app/utils/styles.js
+++ app/utils/styles.js
@@ -26,10 +26,37 @@ const GlobalStyle = createGlobalStyle`
     vertical-align: baseline;
   }
 
-  select, input, textarea {
-    &:active {
-      font-size: 16px;
-    } 
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+  input[type="color"],
+  input[type="date"],
+  input[type="datetime"],
+  input[type="datetime-local"],
+  input[type="email"],
+  input[type="month"],
+  input[type="number"],
+  input[type="password"],
+  input[type="search"],
+  input[type="tel"],
+  input[type="text"],
+  input[type="time"],
+  input[type="url"],
+  input[type="week"],
+  select:focus,
+  textarea {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    font-size: 16px;
+  }
+  textarea:focus,
+  input:focus,
+  button:focus,
+  select:focus {
+    outline: none;
   }
 
   .ant-dropdown {
